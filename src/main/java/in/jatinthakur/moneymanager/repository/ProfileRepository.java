c6ce64be9a03eb817fd10505b6a062760064be48
JatinThakur-797 <jt6285638@gmail.com>
2025-10-24 22:24:48 +0530

    Add ProfileRepository for user profile data access

---
+++ src/main/java/in/jatinthakur/moneymanager/repository/ProfileRepository.java
@@ -0,0 +1,14 @@
+package in.jatinthakur.moneymanager.repository;
+
+
+import in.jatinthakur.moneymanager.entity.ProfileEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
+    Optional<ProfileEntity> findByEmail(String email);
+    Optional<ProfileEntity> findByActivationToken(String activationToken);
+}
