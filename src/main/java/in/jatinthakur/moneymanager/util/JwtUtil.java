2ec740aa6c4a8f307162e2873aae7c6f21718906
JatinThakur-797 <jt6285638@gmail.com>
2025-10-24 22:22:48 +0530

    Add JwtRequestFilter and JwtUtil for JWT authentication handling

---
+++ src/main/java/in/jatinthakur/moneymanager/util/JwtUtil.java
@@ -0,0 +1,85 @@
+package in.jatinthakur.moneymanager.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtUtil {
+
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+    public static final long JWT_TOKEN_VALIDITY = 10 * 60 * 60 * 1000;
+
+
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        // You can add more claims here if needed
+        return createToken(claims, username);
+    }
+
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, (Claims::getExpiration));
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY))
+                // 3. Sign the token with the secure key and algorithm
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    private Key getSigningKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(this.secretKey);
+
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
