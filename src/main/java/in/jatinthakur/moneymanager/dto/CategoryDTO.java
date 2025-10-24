23d6c7bbc4513437eefc3f43ff421a0ae10c8e16
JatinThakur-797 <jt6285638@gmail.com>
2025-10-24 22:21:35 +0530

    Add CategoryDTO class for category data transfer

---
+++ src/main/java/in/jatinthakur/moneymanager/dto/CategoryDTO.java
@@ -0,0 +1,15 @@
+package in.jatinthakur.moneymanager.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class CategoryDTO {
+
+
+}
